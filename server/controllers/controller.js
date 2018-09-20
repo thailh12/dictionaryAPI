5f0097d7b9f2cf57ae26e5a65817bcac3d92adef
Lam Ha Thai <thailh12@gmail.com>
2018-09-20 15:38:19 +0700

    add search api

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,12 +1,25 @@
 const model = require('../../models');
+const Op = model.Sequelize.Op;
 
 function lookUp(req, res) {
-    console.log(req);
     model.Dictionary.find({where: {word: req.params.word}})
         .then(word => res.json(word))
         .catch(e => res.json(e));
 }
 
+function search(req, res) {
+    let a = `^${req.params.string}`;
+    console.log(a);
+    model.Dictionary.findAll({
+        where: {
+            word: {[Op.regexp]: a}
+        }
+    })
+        .then(words => res.json(words))
+        .catch(e => res.json(e));
+}
+
 module.exports = {
+    search,
     lookUp
 };
--- server/routers/index.js
+++ server/routers/index.js
@@ -4,4 +4,7 @@ module.exports = (app) =>{
     app.get('/lookup', (req, res) => {res.send('api ')});
     app.get('/lookup/:word', controller.ctrl.lookUp);
     app.param('word',controller.ctrl.lookUp );
+    app.get('/search', (req, res) =>{ res.json('search')});
+    app.get('/search/:string', controller.ctrl.search);
+    app.param('string', controller.ctrl.search);
 };
