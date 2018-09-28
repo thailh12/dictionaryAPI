b7ee794c2928bd692668895e1e016d4027315ac5
Lam Ha Thai <thailh12@gmail.com>
2018-09-28 21:12:43 +0700

    fix search api

--- models/dictionary.js
+++ models/dictionary.js
@@ -1,13 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-    const Dictionary = sequelize.define('Dictionary', {
-        idx: DataTypes.INTEGER,
-        word: DataTypes.STRING,
-        detail: DataTypes.STRING
-    }, {
-        timestamps: false,
-        freezeTableName: true,
-        tableName: 'en_vi'
-    });
-    return Dictionary;
+  const Dictionary = sequelize.define(
+    'Dictionary',
+    {
+      idx: DataTypes.INTEGER,
+      word: DataTypes.STRING,
+      detail: DataTypes.STRING
+    },
+    {
+      timestamps: false,
+      freezeTableName: true,
+      tableName: 'en_vi'
+    }
+  );
+  return Dictionary;
 };
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,25 +1,25 @@
 const model = require('../../models');
 const Op = model.Sequelize.Op;
+const sequelize = require('../../models').sequelize;
 
 function lookUp(req, res) {
-    model.Dictionary.find({where: {word: req.params.word}})
-        .then(word => res.json(word))
-        .catch(e => res.json(e));
+  model.Dictionary.find({ where: { word: req.params.word } })
+    .then(word => res.json(word))
+    .catch(e => res.json(e));
 }
 
 function search(req, res) {
-    let a = `^${req.params.string}`;
-    console.log(a);
-    model.Dictionary.findAll({
-        where: {
-            word: {[Op.regexp]: a}
-        }
+  let a = `^${req.params.string}`;
+  console.log(a);
+  sequelize
+    .query(`SELECT word FROM dictionary WHERE word REGEXP "${a}"`, {
+      model: model.Dictionary
     })
-        .then(words => res.json(words))
-        .catch(e => res.json(e));
+    .then(words => res.json(words))
+    .catch(e => res.json(e));
 }
 
 module.exports = {
-    search,
-    lookUp
+  search,
+  lookUp
 };
